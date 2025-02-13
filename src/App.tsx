d1617b46f04011d80d4db3b3f08d2f7fec88df2c
jeremyobiangdev <jeremyobiangdev@gmail.com>
2025-02-13 18:27:46 +0100

    feat: Added transition display + design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { motion, usePresenceData } from 'motion/react'
+import { motion } from 'motion/react'
 import './App.css'
 
 function App() {
-  const direction = usePresenceData()
 
   return (
     <>
       <div
-        className="flex flex-col w-full items-start py-12 px-2 gap-y-12"
+        className="flex flex-col w-full h-screen items-start py-19 px-8 gap-y-6"
       >
-        <div className="leading-14">
+        <div className="leading-14 tracking-tighter">
           <motion.div
             className="text-[3rem] overflow-hidden"
           >
@@ -43,8 +42,13 @@ function App() {
             </motion.h1>
           </motion.div>
         </div>
+        <div 
+          className="flex justify-end items-center w-full"
+        >
+
+        </div>
         <motion.div 
-          className="w-full grid grid-cols-3 grid-rows-1 gap-0.5 p-2 bg-amber-200"
+          className="w-full grid grid-cols-3 grid-rows-1 gap-0.5 p-2 gap-x-5"
           initial={{ opacity: 0, y: 50 }}
               animate={{
                   opacity: 1,
@@ -54,8 +58,34 @@ function App() {
                   },
               }}
         >
-          <motion.div>
-
+          <motion.div
+            className="bg-gray-200 h-[60vh] rounded-lg"
+          >
+            <motion.div
+              className="z-0 flex flex-col justify-between items-start w-full h-full rounded-lg p-4"
+            >
+              <h1
+                className="text-xl font-bold text-gray-500 font-mono"
+              >
+                01
+              </h1>
+              <div 
+                className="flex flex-col w-full gap-y-2"
+              >
+                <h1
+                  className="text-[1.75rem] font-[500] text-black font-mono"
+                >
+                  Speed
+                </h1>
+                <div 
+                  className="flex"
+                >
+                  <p className="text-sm font-[500] text-black rounded-full bg-white p-2">Current Speed</p>
+                  <p className="text-sm font-[500] text-black rounded-full bg-white p-2">Averrage Speed</p>
+                  <p className="text-sm font-[500] text-black rounded-full bg-white p-2">Max Speed</p>
+                </div>
+              </div>
+            </motion.div>
           </motion.div>
         </motion.div>
       </div>
